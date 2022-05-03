cc2ca031e9430ecc943eb9db4338d99d19394d1c
Heike <heike.seegruber@ci4rail.com>
2022-05-03 16:36:43 +0200

    Set node name in provisioning step.

--- meta-edgefarm-core/recipes-containers/edgecore/files/configure_edgecore.sh
+++ meta-edgefarm-core/recipes-containers/edgecore/files/configure_edgecore.sh
@@ -5,6 +5,7 @@
 # To be called with arguments: <cloudcore address> <node name>
 #
 # Generate and apply edgecore config file.
+# Set host name to <node name>.
 # Do not forget to provide /etc/kubeedge/certs/rootCa.pem and /etc/kubeedge/certs/node.pem for a successful connection.
 
 if [ "$#" -ne 2 ] ; then
@@ -12,14 +13,20 @@ if [ "$#" -ne 2 ] ; then
     exit 1
 fi
 
+export NODE_NAME=${2}
+
+# Update hostname
+oldHostname=$(cat /etc/hostname)
+hostnamectl set-hostname $NODE_NAME
+sed -i "s/$oldHostname/$NODE_NAME/g" /etc/hosts
+
+# Generate edgecore config
 template=/etc/kubeedge/config/edgecore.yaml.TEMPLATE
 
 case "${1}" in
 *.nip.io*) export CLOUDCORE_ADDRESS=$(echo ${1} | sed -e 's/\./\-/g' -e 's/-nip-io/.nip.io/g') ;;
 *        ) export CLOUDCORE_ADDRESS=${1} ;;
 esac
-
-export  NODE_NAME=${2}
 
 eval "echo \"$(cat "${template}")\"" > /etc/kubeedge/config/edgecore.yaml
 
