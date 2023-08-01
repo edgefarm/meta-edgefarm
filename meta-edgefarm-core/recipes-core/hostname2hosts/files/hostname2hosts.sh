099bbf482a99340ddddfd77b10100fdadef0d20b
KlausPopp <klauspopp@gmx.de>
2023-08-01 10:20:11 +0200

    fix hostname2hosts.sh

--- meta-edgefarm-core/recipes-core/hostname2hosts/files/hostname2hosts.sh
+++ meta-edgefarm-core/recipes-core/hostname2hosts/files/hostname2hosts.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # This script is used to update the /etc/hosts: The hostname found in /etc/hostname shall be added to the localhost IPv4 address (127.0.0.1).
-#hostname_file="/etc/hostname"
-hostname_file="hostname"
+hostname_file="/etc/hostname"
 ip_address="127.0.0.1"
-#hosts_file="/etc/hosts"
-hosts_file="hosts"
+hosts_file="/etc/hosts"
 
 
 # Check if the hostname file exists
 if [ ! -f "$hostname_file" ]; then
     echo "Hostname file not found."
-    exit 1
+    exit 0
 fi
 
 # Read the hostname from the file
@@ -20,7 +18,7 @@ hostname=$(cat $hostname_file)
 # test if hostname is empty
 if [ -z "$hostname" ]; then
     echo "Hostname file is empty."
-    exit 1
+    exit 0
 fi
 
 # Check if the IP address is already in the /etc/hosts file
